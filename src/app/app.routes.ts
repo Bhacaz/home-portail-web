2e5d175e6e2f6798f6ec74acf08fc88692628037
Jean-Francis Bastien <bhacaz@gmail.com>
2018-01-28 15:27:27 -0500

    Update flex to row when small display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import {Routes} from "@angular/router";
 import {ChromecastListComponent} from "./chromecast-list/chromecast-list.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {LocalDeviceListComponent} from "./local-device-list/local-device-list.component";
+import {WeatherForecastComponent} from "./conditions/weather/weather-forecast/weather-forecast.component";
 
 export const appRoutes: Routes = [
   { path: 'cast', component: ChromecastListComponent },
+  { path: 'local-device', component: LocalDeviceListComponent },
+  { path: 'weather', component: WeatherForecastComponent },
   { path: '', component: DashboardComponent },
   { path: '**', redirectTo: '' }
 ];
